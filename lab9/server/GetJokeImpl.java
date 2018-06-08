ad68e58fad75b2196f049b038d723390f6b8dfca
walkersorlie <walkersorlie@gmail.com>
2018-06-08 11:18:34 -0600

    Lab9 zip

--- lab9/server/GetJokeImpl.java
+++ lab9/server/GetJokeImpl.java
@@ -9,7 +9,7 @@ public class GetJokeImpl extends UnicastRemoteObject implements GetJoke {
     private String joke3 = "Why was King Arthur's army too tired to fight?\r\nIt has too many sleepless knights.";
     private String joke4 = "An SEO expert walks into a bar, bars, pub, tavern, public house, Irish pub, drinks, beer, alcohol";
     private String joke5 = "What is the biggest lie in the entire universe?\r\n\"I have read and agree to the Terms and Conditions.\"";
-    
+
     private String list[] = {joke1, joke2, joke3, joke4, joke5};
 
     public GetJokeImpl() throws RemoteException { }
